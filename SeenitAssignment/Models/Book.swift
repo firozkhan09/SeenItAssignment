1aa2b6dfdc71131e4ee87b5c5872db2304107ce6
Firoz <fkhan@workllama.com>
2021-08-10 00:55:20 +0530

    Model Created

---
+++ SeenitAssignment/Models/Book.swift
@@ -0,0 +1,17 @@
+//
+//  Book.swift
+//  SeenitAssignment
+//
+//  Created by Firoz khan on 10/08/21.
+//
+
+import Foundation
+struct Book {
+    var id:Int?
+    var title:String?
+    var isbn:String?
+    var price:Int?
+    var currencyCode:String?
+    var author:String?
+    var description:String?
+}
